b6d19cc4f3313befcce1b251d30e3068043f31fe
RaghavBansalMCS <Raghav.Bansal.MCS@outlook.com>
2024-03-05 13:32:29 -0500

    Initial Commit

---
+++ LoginScreen/LoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenApp.swift
+//  LoginScreen
+//
+//  Created by Consultant on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
